f09673bcbe064561b79fe5fab9e83573f9cef527
Miguel P <87776778+MiguelParraC@users.noreply.github.com>
2024-02-08 17:21:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
       </header>
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
