3c7ecb9f696fa1b24f33aeec9b41fc9435d42e28
Miguel P <87776778+MiguelParraC@users.noreply.github.com>
2024-02-14 17:14:27 -0600

    1

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -5,7 +5,7 @@ import './TodoCounter.css';
 function TodoCounter({ total, completed }) {
   return (
     // se abren llaves para enviar variables y las segundas son para el objeto
-    <h1> 
+    <h1 className="TodoCounter"> 
       Has completado {completed} de {total} ToDo
     </h1>
   );
